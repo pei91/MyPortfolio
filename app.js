6f5c9532e117e306004c32edc5793ff0b9dff918
Pei <peichuan@buffalo.edu>
2020-10-26 17:48:17 -0400

    'g'

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
